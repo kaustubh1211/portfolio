f8ef655d886d665284f083080d3e9789226a854a
Kaustubh Patil <kaustubhpatil1211@gmail.com>
2025-12-24 15:42:10 +0530

    next -v updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  experimental: {
+    turbo: true,  // Enable Turbopack if you want to use it
+  },
 };
-
-export default nextConfig;
