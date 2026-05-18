5934f64ea4f6caed0391d564f6925b11abcb6014
Kaustubh Patil <kaustubhpatil1211@gmail.com>
2026-05-18 21:56:47 +0530

    added border on container

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
-export default function Container({
-  children,
-  className,
-  ...props
-}: {
+interface ContainerProps extends React.ComponentPropsWithoutRef<'div'> {
   children: React.ReactNode;
   className?: string;
-}) {
+}
+
+export default function Container({
+  children,
+  className = '',
+  ...props
+}: ContainerProps) {
   return (
-    < div
-      className={`container mx-auto max-w-3xl px-4 animate-fade-in-blur  border-gray-800 ${className}`}
-      {...props}
-    >
-      {children}
+    <div className="relative w-full border-b border-t border-zinc-200/80 dark:border-zinc-800/80">
+      {/* Background/Layout wrapper that limits content width and adds vertical borders */}
+      <div
+        className={`
+          container mx-auto max-w-3xl px-4 animate-fade-in-blur
+          border-x border-zinc-200/80 dark:border-zinc-800/80
+          ${className}
+        `}
+        {...props}
+      >
+        {children}
+      </div>
     </div>
   );
 }
